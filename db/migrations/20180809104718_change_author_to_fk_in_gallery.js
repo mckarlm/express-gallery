510b2cc48b533a463171d50cd8a457cbe1546998
Mckarl Manuel <manmckarl@gmail.com>
2018-08-09 11:05:01 -1000

    added user_id to gallery to reference id in users table

--- db/migrations/20180809104718_change_author_to_fk_in_gallery.js
+++ db/migrations/20180809104718_change_author_to_fk_in_gallery.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema
+  return knex.schema.table('gallery', table =>{
+    table.integer('user_id').references('id').inTable('users')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.table('gallery', table =>{
+    table.dropColumn('user_id');
+  })
 };
